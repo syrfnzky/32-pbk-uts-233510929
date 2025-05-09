1284754d32cde717e1bf01f5588dc6353575ea34
syrfnzky <syirfanzaky@student.uir.ac.id>
2025-05-09 23:02:07 +0700

    info and filter task

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const tasks = ref([])
 const input = ref('')
+const filter = ref('all')
 
 const addTask = () => {
   if (input.value.trim() === '') return
@@ -18,20 +19,47 @@ const removeTask = (id) => {
   tasks.value = tasks.value.filter(task => task.id !== id)
 }
 
+const countCompletedTasks = computed(() => {
+  return tasks.value.filter(task => task.completed).length
+})
+
+const countTask = computed(() => {
+  return tasks.value.length
+})
+
+const filteredTasks = computed(() => {
+  if (filter.value === 'all') {
+    return tasks.value
+  } else if (filter.value === 'completed') {
+    return tasks.value.filter(task => task.completed)
+  } else if (filter.value === 'active') {
+    return tasks.value.filter(task => !task.completed)
+  }
+  return []
+})
+
 </script>
 
 <template>
   <div>
     <input type="text" v-model="input" @keyup.enter="addTask" />
     <button @click="addTask">➕ Add Task</button>
+    <div>
+      <button @click="filter = 'all'">All</button>
+      <button @click="filter = 'completed'">Completed</button>
+      <button @click="filter = 'active'">Active</button>
+    </div>
 
     <ul>
-      <li v-for="task in tasks" :key="task.id">
+      <li v-for="task in filteredTasks" :key="task.id">
         <input type="checkbox" v-model="task.completed" />
         {{ task.text }}
         <button @click="removeTask(task.id)">❌</button>
       </li>
     </ul>
+
+    <p>Completed Tasks: {{ countCompletedTasks }}</p>
+    <p>Total Tasks: {{ countTask }}</p>
   </div>
 </template>
 
