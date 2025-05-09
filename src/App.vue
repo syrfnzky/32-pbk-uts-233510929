263814b10d6565a31d844388ed2d8c6c1f14cb2b
syrfnzky <syirfanzaky@student.uir.ac.id>
2025-05-09 22:57:01 +0700

    input task

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script setup>
+import { ref } from 'vue'
+
+const tasks = ref([])
+const input = ref('')
+
+const addTask = () => {
+  if (input.value.trim() === '') return
+  tasks.value.push({
+    id: tasks.value.length + 1,
+    text: input.value,
+    completed: false
+  })
+  input.value = ''
+}
 </script>
 
 <template>
   <div>
-    
+    <input type="text" v-model="input" @keyup.enter="addTask" />
+    <button @click="addTask">➕ Add Task</button>
   </div>
 </template>
 
